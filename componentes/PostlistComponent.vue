71ebbcacfce34d9ac88ea18c808b7c9f99157710
Delmimv <124009348+Delmimv@users.noreply.github.com>
2023-05-30 10:20:17 -0600

    Add files via upload

---
+++ componentes/PostlistComponent.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="container">
+<h1>Post</h1>
+<br />
+<ul v-for="post in posts" :key="post.idi">
+<li>{{post.id}} {{post.post_name}}</li>
+</ul>
+</div>
+
+
+
+</template>
+
+<script>
+export default([
+    data(){
+        posts: []
+    },
+    methods:{
+        getPost(api_url){
+            api_url = api_url || '/api/v2/post'
+            const headers ={
+                'Authorization': 'Bearer 1|
+
+            };
+            fetch(api_url, (headers))
+            .then(response=>response.json())
+            .ten(response=>{
+                this.post=response.data
+            })
+            .catch(error=>console.log(error))
+        }
+    }
+])
---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import {createApp} from 'vue';
+
+import PostslistComponent from './components/PostslistComponent.vue'
+
+createApp(PostslistComponent)
